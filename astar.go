78bb39eec44af2333daa77625bc57f0987188702
prots <prots.igor@gmail.com>
2016-12-10 12:44:43 +0200

    intermediate commit

--- astar.go
+++ astar.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-type Point struct{
-	x int
-	y int
+type Point struct {
+	x           int
+	y           int
 	obstruction int
-	parent *Point
+	parent      *Point
 }
 
 // 5. Create an empty "opened" list of Points
@@ -16,14 +17,14 @@ type Point struct{
 var openedList = make([]*Point, 0)
 var closedList = make([]*Point, 0)
 
-const(
+const (
 	defObstruction = 0
-	maxNeigbours = 8
-	directWeight = 10
+	maxNeigbours   = 8
+	directWeight   = 10
 	diagonalWeight = 14
 )
 
-func newPoint(xi, yj, obstruction int) Point{
+func newPoint(xi, yj, obstruction int) Point {
 	return Point{x: xi, y: yj, obstruction: obstruction}
 }
 
@@ -32,9 +33,9 @@ func main() {
 	// 1. Setup empty field
 	size := 10
 	field := make([][]Point, size)
-	for xi:=0;xi<size;xi++{
+	for xi := 0; xi < size; xi++ {
 		field[xi] = make([]Point, size)
-		for yj:=0;yj<size;yj++{
+		for yj := 0; yj < size; yj++ {
 			field[xi][yj] = newPoint(xi, yj, defObstruction)
 		}
 	}
@@ -62,7 +63,7 @@ func main() {
 	fmt.Printf("Neighbours: %v\n", neighbours)
 	// 10. Remove start point from the openedList and add it to the closedList
 	fmt.Printf("openedList: %v\n", openedList)
-	openedList = openedList[0:len(openedList)-1]
+	openedList = openedList[0 : len(openedList)-1]
 	fmt.Printf("openedList: %v\n", openedList)
 	closedList = append(closedList, &startPoint)
 	fmt.Printf("closedList: %v\n", closedList)
@@ -83,36 +84,36 @@ func findNeighbours(point *Point, field [][]Point) []*Point {
 	neigbourghs := make([]*Point, 0)
 	fmt.Println("point: ", &point)
 	if point.x >= 1 && point.y >= 1 {
-		for i:=(point.x - 1); i <= (point.x + 1); i++{
-			for j:=(point.y - 1); j <= (point.y + 1); j++ {
-				if (field[i][j].obstruction == 0  && !(point.x == i && point.y == j)) {
+		for i := (point.x - 1); i <= (point.x + 1); i++ {
+			for j := (point.y - 1); j <= (point.y + 1); j++ {
+				if field[i][j].obstruction == 0 && !(point.x == i && point.y == j) {
 					neigbourghs = append(neigbourghs, &field[i][j])
 				}
 			}
 		}
 	}
-	if point.x >=1 && point.y == 0 {
-		for i:=(point.x - 1); i <= (point.x + 1); i++{
-			for j:= point.y; j <= (point.y + 1); j++ {
-				if (field[i][j].obstruction == 0  && !(point.x == i && point.y == j)) {
+	if point.x >= 1 && point.y == 0 {
+		for i := (point.x - 1); i <= (point.x + 1); i++ {
+			for j := point.y; j <= (point.y + 1); j++ {
+				if field[i][j].obstruction == 0 && !(point.x == i && point.y == j) {
 					neigbourghs = append(neigbourghs, &field[i][j])
 				}
 			}
 		}
 	}
 	if point.x == 0 && point.y >= 1 {
-		for i:= point.x; i <= (point.x + 1); i++{
-			for j:=(point.y - 1); j <= (point.y + 1); j++ {
-				if (field[i][j].obstruction == 0  && !(point.x == i && point.y == j)) {
+		for i := point.x; i <= (point.x + 1); i++ {
+			for j := (point.y - 1); j <= (point.y + 1); j++ {
+				if field[i][j].obstruction == 0 && !(point.x == i && point.y == j) {
 					neigbourghs = append(neigbourghs, &field[i][j])
 				}
 			}
 		}
 	}
 	if point.x == 0 && point.y == 0 {
-		for i:= point.x; i <= (point.x + 1); i++{
-			for j:=point.y; j <= (point.y + 1); j++ {
-				if (field[i][j].obstruction == 0  && !(point.x == i && point.y == j)) {
+		for i := point.x; i <= (point.x + 1); i++ {
+			for j := point.y; j <= (point.y + 1); j++ {
+				if field[i][j].obstruction == 0 && !(point.x == i && point.y == j) {
 					neigbourghs = append(neigbourghs, &field[i][j])
 				}
 			}
@@ -121,8 +122,39 @@ func findNeighbours(point *Point, field [][]Point) []*Point {
 	return neigbourghs
 }
 
-func manhattenDistance(start, end *Point) int {
-	distance := 0
+func manhattenDistance(start, end *Point) float64 {
+	return math.Abs((float64)(end.x - start.x)) + math.Abs((float64)(end.y - start.y))
+}
 
-	return distance
+func evklidDistance(start, end *Point) float64 {
+	dx := math.Abs((float64)(end.x - start.x))
+	dy := math.Abs((float64)(end.y - start.y))
+	return math.Hypot(dx, dy)
+}
+
+//func findNextPoint(start *Point) {
+//	for _, neighbour := range openedList {
+//		findWeight(start, neighbour)
+//	}
+//}
+
+//func findWeight(start, finish, current *Point) float64 {
+//
+//}
+
+func getBackRoute(start, end *Point) int {
+	route := 0
+	if start == end {
+		return 0
+	}
+	if end.parent == nil {
+		return 0
+	}
+	if manhattenDistance(end, end.parent) == 1 {
+		route = 10
+	}
+	if manhattenDistance(end, end.parent) == 2 {
+		route = 14
+	}
+	return route + getBackRoute(start, end.parent)
 }
